5eb17f3a0434cea7005668a2ee8896bd0355ce50
Matheus Augusto Paschuinio <matheus.paschuinio@fatec.sp.gov.br>
2025-09-25 18:04:37 -0300

    Função

---
+++ dart/aula13.dart
@@ -0,0 +1,16 @@
+void main() {
+  // chamando funções
+  print(getNome());
+
+  var pegaFuncao = getNumero();
+
+  print(pegaFuncao);
+}
+
+String getNome() {
+  return "Matheus";
+}
+
+int getNumero() {
+  return 150;
+}
